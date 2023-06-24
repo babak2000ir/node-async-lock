cbdb168a6248da56f2d511efa53a6db78246c21c
Babak Jahedmanesh <babak.jahedmanesh@thenavpeople.co.uk>
2023-06-24 21:02:24 +0100

    comparison scenario

--- app.js
+++ app.js
@@ -1,61 +1,50 @@
 import AsyncLock from 'async-lock';
 const lock = new AsyncLock();
 
-const sharedData = {
-    varA: 0,
-    varB: 0,
-    varC: 0,
+let sharedData = 0;
 
-    getData(key) {
-        return this[key];
-    },
+function operation1() {
+    return new Promise((resolve, reject) => {
+        lock.acquire('lock1', function () {
+            console.log('op1:lock');
 
-    setData(key, value) {
-        const sdThis = this;
-        lock.acquire(key, function () {
-            // Concurrency safe
-            sdThis[key] = value;
+            setTimeout(() => {
+                sharedData = sharedData + 1;
+            }, 1000);
         }, function (err, ret) {
+            console.log('operation1-done');
+            resolve();
         });
-    }
-}
-
-// Helper function simulating an asynchronous operation
-function someAsyncOperation() {
-    return new Promise((resolve) => {
-        setTimeout(resolve, 1000);
     });
 }
 
-function operation1() {
-    let varA = sharedData.getData('varA');
-    console.log('op1: recieved varA:', varA);
-    sharedData.setData('varA', varA + 1);
-    varA = sharedData.getData('varA');
-    console.log('op1: set varA:', varA + 1);
-}
-
 function operation2() {
-    let varA = sharedData.getData('varA');
-    console.log('op2: recieved varA:', varA);
-    sharedData.setData('varA', varA + 1);
-    varA = sharedData.getData('varA');
-    console.log('op2: set varA:', varA + 1);
+    return new Promise((resolve, reject) => {
+        lock.acquire('lock2', function () { //lock1/lock2 to compare
+            console.log('op2:lock');
+
+            setTimeout(() => {
+                sharedData = sharedData + 1;
+            }, 2000);
+        }, function (err, ret) {
+            console.log('operation2-done');
+            resolve();
+        });
+    });
 }
 
 // Usage example
 async function main() {
-    console.log('Shared data before:' + JSON.stringify(sharedData, null, 2));
+    const start = Date.now();
+    console.log('Shared data before:' + sharedData);
 
-    /* await Promise.all([
+    await Promise.all([
         operation1(),
         operation2(),
-    ]); */
+    ]);
 
-    await operation1();
-    await operation2();
-
-    console.log('Shared data after:' + JSON.stringify(sharedData, null, 2));
+    console.log('Shared data after: ' + sharedData);
+    console.log('Time elapsed:' + (Date.now() - start));
 }
 
 main();
